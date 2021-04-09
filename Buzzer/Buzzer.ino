4a4bef5f4888ea4edad861248ae0a8f9b1514c22
Walter Xu <62961275+xuyunong720@users.noreply.github.com>
2021-04-09 19:58:03 +1200

    successful buzzer

--- Buzzer/Buzzer.ino
+++ Buzzer/Buzzer.ino
@@ -1,40 +1,50 @@
-/*
-TaskHandle_t Buzzer1;
-TaskHandle_t Buzzer2;
+#include "Pitches.h" 
 
-int buzzer1_pin = 14;
-int buzzer2_pin = 4;
 
-int tempo = 130;
-int tempo2 = 70;
+
 
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(buzzer1_pin, OUTPUT);
-  pinMode(buzzer2_pin, OUTPUT);
-
-  xTaskCreatePinnedToCore(
-    Buzzer1_code, 
-    "Buzzer1", 
-    10000,  
-    NULL, 
-    1,  
-    &Buzzer1,  
-    0); 
-    
-  xTaskCreatePinnedToCore(
-    Buzzer2_code, 
-    "Buzzer2", 
-    10000, 
-    NULL, 
-    1,  
-    &Buzzer2,  
-    1); 
-
-}
-
-void loop() {
   //fuck
 }
-*/
+
+
+void loop() {
+
+int melody[] = {NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_D5, NOTE_D5, NOTE_D5,NOTE_D5, NOTE_G5, NOTE_G5, NOTE_G5,
+NOTE_G5, NOTE_A5, NOTE_A5, NOTE_A5,NOTE_A5, NOTE_A5, NOTE_A5, NOTE_A5,NOTE_A5, NOTE_A5,NOTE_A5, NOTE_A5,NOTE_A5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_G4};
+
+//note durations. 4=quarter note / 8=eighth note
+int noteDurations[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  4, 4, 4, 4, 4, 4, 4, 4};
+
+  for (int thisNote = 0; thisNote < 36; thisNote++) {
+
+    int noteDuration = 800 / noteDurations [thisNote];
+    tone(8, melody [thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+    //stop the tone playing
+    noTone(8);
+  }
+
+//-----------------------------------------------------------------------------------------------------------------------------
+  int melody2[] = {NOTE_A4, 0, NOTE_A4, NOTE_E5, NOTE_D5, 0, NOTE_C5, 0, NOTE_B4, 0, NOTE_B4,NOTE_B4, NOTE_D5, 0, NOTE_C5,
+NOTE_B4, NOTE_A4, 0, NOTE_A4, NOTE_C6, NOTE_B5, NOTE_C6, NOTE_B5, NOTE_C6, NOTE_A4, 0, NOTE_A4, NOTE_C6, NOTE_B5, NOTE_C6, NOTE_B5, NOTE_C6};
+
+//note durations. 4=quarter note / 8=eighth note
+int noteDuration2[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+
+  for (int thisNote = 0; thisNote < 32; thisNote++) {
+
+    int noteDuration = 800 / noteDuration2 [thisNote];
+    tone(8, melody2 [thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+    //stop the tone playing
+    noTone(8);
+  }
+
+}
